9ecf43dd661549cdd2f9dedc46d4960361cdaa46
flandre scarlet <flantan233@gmail.com>
2018-05-16 15:12:43 +0800

    change reouter

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     { path: '/', name: '含Menu的所有页面（自动路由首页）', component: MenuSelect, children: [
-        { path: '/index', name: '首页', component: Index },
+        { path: '/', name: '首页', component: Index },
         { path: '/study', name: '学习', component: Study },
         { path: '/find', name: '发现', component: Find },
         { path: '/home', name: '我的', component: Home }
