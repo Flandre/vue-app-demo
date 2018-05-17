d325c18cf045e91c31cb5186ac89305c7c5ec3a1
flandre scarlet <flantan233@gmail.com>
2018-05-17 18:24:33 +0800

    init class component

---
+++ src/components/ClassCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="class-card">
+    <div class="title">
+      推荐学习
+    </div>
+    <swiper :options="swiperOption" ref="classSwiper">
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import 'swiper/dist/css/swiper.css'
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  export default {
+    name: "class-card",
+    data() {
+      return {
+        swiperOption :{
+          slidesPerView :'auto',
+          centeredSlides : true,
+        },
+      }
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
