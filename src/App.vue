6a385e017b1016827849a908ebf2948dad7b0361
flandre scarlet <flantan233@gmail.com>
2018-05-18 17:41:05 +0800

    add cube

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,12 @@ export default {
 body {
   background: #EFF3F7
 }
+.cube {
+  -webkit-transform: translateZ(0);
+  -moz-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  -o-transform: translateZ(0);
+  transform: translateZ(0);
+  /* Other transform properties here */
+}
 </style>
