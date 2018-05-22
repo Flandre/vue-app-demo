060a40381240c1370c43251d8c876d729a9b375f
flandre scarlet <flantan233@gmail.com>
2018-05-22 16:29:50 +0800

    add less

--- src/pages/CreditKnow.vue
+++ src/pages/CreditKnow.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="credit-know">
     <NavigatorBar title="了解分数" bgColor="#45A4F7"/>
+    <div class="know-container">
+      <div class="know-info-container">
+
+      </div>
+      <img src="../assets/pages/creditKnow/bg-border.png" alt="">
+    </div>
     <div class="menu-item" v-for="i in creditType">
       <router-link :class="['item-box', i.showClass]" active-class="active" :to="i.url" replace>
         <span>{{i.name}}</span>
       </router-link>
     </div>
-    <div >当前状态：？？？</div>
+    <div >{{type}}</div>
     <router-view/>
     <p>了解分数页面</p>
   </div>
@@ -45,14 +51,11 @@
             url: '/creditKnow/conduct',
             showClass: 'conduct'
           }
-        ]
+        ],
+        type: 'identity'
       }
     },
     mounted() {
-      this.$nextTick(function () {
-        // Code that will run only after the
-        // entire view has been rendered
-      })
       /* 返回顶部 */
       window.scrollTo(0, 0)
     },
@@ -60,15 +63,24 @@
       NavigatorBar
     },
     beforeRouteUpdate (to, from, next) {
-      console.log(to)
-      console.log(from)
+      this.creditType.forEach(val => {
+        if(to.fullPath == val.url)
+          this.type =  val.showClass
+      })
+      console.log(this.type)
       next()
     }
   }
 </script>
-
-<style scoped>
+<style lang="less" scoped>
   .credit-know {
     margin-top: 44px;
+    .know-container {
+      background: #fff;
+      .know-info-container {
+        height: 144px;
+        background: #45A4F7
+      }
+    }
   }
 </style>
