6dc356baddc3277690168f129e411056bbedf4c7
flandre scarlet <flantan233@gmail.com>
2018-05-25 16:45:02 +0800

    update manager page

--- src/pages/ManagerIndex.vue
+++ src/pages/ManagerIndex.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="manager-index">
+    <NavigatorBar title="车辆管家" bgColor="#FBCB61"/>
 
   </div>
 </template>
 
 <script>
+  import NavigatorBar from '@/components/NavigatorBar/NavigatorBar'
   export default {
-    name: "manager-index"
+    name: "manager-index",
+    components: {
+      NavigatorBar
+    },
+    mounted() {
+      /* 返回顶部 */
+      window.scrollTo(0, 0)
+    },
   }
 </script>
 
