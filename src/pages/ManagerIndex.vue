ae7556faa1f8c7be297c686c528ec8067ff11665
flandre scarlet <flantan233@gmail.com>
2018-05-28 11:06:52 +0800

    update page

--- src/pages/ManagerIndex.vue
+++ src/pages/ManagerIndex.vue
@@ -5,6 +5,67 @@
       <div class="car-panel">
         <p class="car-num">{{lsNum}}</p>
         <p class="car-type">{{carType}}</p>
+        <img src="../assets/pages/ManagerIndex/carType.png" class="car-thumb">
+      </div>
+    </div>
+    <div class="violation-reminding panel-item">
+      <p class="title">违章提醒</p>
+      <template v-if="violationArr.length == 0">
+        <p class="info">暂无新的违章或尚未录入</p>
+      </template>
+      <template v-else>
+        <div class="info-main">
+          <div class="info-item">
+            <p class="num">{{violationArr[0]}}</p>
+            <p class="proj">违章</p>
+          </div>
+          <div class="info-item">
+            <p class="num">{{violationArr[1]}}</p>
+            <p class="proj">罚款</p>
+          </div>
+          <div class="info-item">
+            <p class="num">{{violationArr[2]}}</p>
+            <p class="proj">扣分</p>
+          </div>
+        </div>
+      </template>
+      <p class="last-time">上次查询时间：2018-03-01 09:00</p>
+    </div>
+    <div class="inspection panel-item">
+      <p class="title">年检提醒</p>
+      <div class="info-main" v-if="inspection > 0">
+        <div class="info-item">
+          <p class="num" v-if="inspection == 1">373<em>天</em></p>
+          <p class="num warn" v-else-if="inspection == 2">12<em>天</em></p>
+          <p class="proj">下次领标日期：2019-03-11</p>
+        </div>
+      </div>
+      <div class="info-main" v-else>
+        <div class="info-item">
+          <img src="../assets/pages/ManagerIndex/add.png" class="add">
+          <p class="proj">轻松设置，年检及时办</p>
+        </div>
+      </div>
+    </div>
+    <div class="insurance panel-item">
+      <p class="title">保险提醒</p>
+      <div class="info-main" v-if="insurance > 0">
+        <div class="info-item">
+          <p class="num" v-if="insurance == 1">100<em>天</em></p>
+          <p class="num warn" v-else-if="insurance == 2">24<em>天</em></p>
+          <p class="proj">交强险到期日：2019-09-01</p>
+        </div>
+        <div class="info-item">
+          <p class="num" v-if="insurance == 1">100<em>天</em></p>
+          <p class="num warn" v-else-if="insurance == 2">24<em>天</em></p>
+          <p class="proj">商业险到期日：2019-09-01</p>
+        </div>
+      </div>
+      <div class="info-main" v-else>
+        <div class="info-item">
+          <img src="../assets/pages/ManagerIndex/add.png" class="add">
+          <p class="proj">到期提醒，一键报案</p>
+        </div>
       </div>
     </div>
   </div>
@@ -18,7 +79,10 @@
       return {
         bgColor: '#45A4F7',
         lsNum: '京QY96N0',
-        carType: '广汽 欧蓝德'
+        carType: '广汽 欧蓝德',
+        violationArr: [],
+        inspection: 0,
+        insurance: 0,
       }
     },
     components: {
@@ -29,9 +93,18 @@
       window.scrollTo(0, 0)
       if(this.$route.query.lsNum){
         this.lsNum = `京${this.$route.query.lsNum}`
+        if(this.$route.query.lsNum != 'QY96N0'){
+          this.violationArr = [4, 600, 6]
+        }
       }
       if(this.$route.query.carType){
         this.carType = this.$route.query.carType
+      }
+      if(this.$route.query.inspection){
+        this.inspection = this.$route.query.inspection
+      }
+      if(this.$route.query.insurance){
+        this.insurance = this.$route.query.insurance
       }
     },
   }
@@ -44,12 +117,127 @@
       height: 110px;
       background: #45A4F7;
       padding: 15px 20px;
+      margin-bottom: 30px;
       .car-panel {
         height: 110px;
         background: #fff;
         border-radius: 8px;
         padding: 30px 20px;
         position: relative;
+        .car-num {
+          color: #333333;
+          font-size: 22px;
+          line-height: 22px;
+          margin-bottom: 17px;
+        }
+        .car-type {
+          color: #333333;
+          font-size: 13px;
+          line-height: 13px;
+        }
+        .car-thumb {
+          width: 101px;
+          height: 46px;
+          position: absolute;
+          top: 40px;
+          right: 30px;
+        }
+      }
+    }
+    .panel-item {
+      background: #fff;
+      padding: 10px 15px 25px;
+      margin-bottom: 10px;
+      .title {
+        font-size: 13px;
+        line-height: 16px;
+        color: #333;
+        padding-left: 26px;
+        margin-bottom: 25px;
+        background-size: 14px 14px;
+        background-repeat: no-repeat;
+        background-position: left center;
+      }
+      .info-main {
+        display: flex;
+        justify-content: space-around;
+        .info-item {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+      }
+    }
+    .violation-reminding {
+      .title {
+        background-image: url("../assets/pages/ManagerIndex/violation.png");
+      }
+      .info {
+        font-size: 13px;
+        line-height: 13px;
+        text-align: center;
+        color: #333;
+        margin-bottom: 16px;
+      }
+      .info-main {
+        margin-bottom: 16px;
+        .info-item {
+          .num {
+            font-size: 20px;
+            line-height: 20px;
+            color: #333;
+            margin-bottom: 10px;
+          }
+          .proj {
+            font-size: 12px;
+            line-height: 12px;
+            color: #666666;
+          }
+        }
+      }
+      .last-time {
+        font-size: 12px;
+        text-align: center;
+        color: #666;
+      }
+    }
+    .inspection,
+    .insurance {
+      .title {
+        background-image: url("../assets/pages/ManagerIndex/inspection.png");
+      }
+      .info-main {
+        .info-item {
+          .num {
+            font-size: 20px;
+            line-height: 20px;
+            color: #333;
+            margin-bottom: 10px;
+            em {
+              font-size: 12px;
+              font-style: normal;
+              margin-left: 5px;
+            }
+            &.warn {
+              color: #D84040;
+            }
+          }
+          .proj {
+            font-size: 12px;
+            line-height: 12px;
+            color: #666666;
+          }
+          .add {
+            width: 36px;
+            height: 36px;
+            margin-bottom: 15px;
+          }
+        }
+      }
+    }
+    .insurance {
+      .title {
+        background-image: url("../assets/pages/ManagerIndex/inspection.png");
       }
     }
   }
