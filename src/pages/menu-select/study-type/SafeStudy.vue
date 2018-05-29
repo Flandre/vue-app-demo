563e89a1d2ff634b9d2c6b1d7e716968fe45e572
flandre scarlet <flantan233@gmail.com>
2018-05-29 16:58:24 +0800

    new page

--- src/pages/menu-select/study-type/SafeStudy.vue
+++ src/pages/menu-select/study-type/SafeStudy.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="safe-study">
-    这是安全驾驶
+    <StudyRecommend dataSource="210"/>
+    <StudyGroup dataSource="11_22222_00_120"/>
   </div>
 </template>
 
 <script>
+  import StudyRecommend from '@/components/StudyRecommend/StudyRecommend'
+  import StudyGroup from '@/components/StudyGroup/StudyGroup'
   export default {
-    name: "safe-study"
+    name: "safe-study",
+    components: {
+      StudyRecommend,
+      StudyGroup
+    }
   }
 </script>
 
