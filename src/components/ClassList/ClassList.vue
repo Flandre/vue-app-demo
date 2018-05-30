66c3b3b276bdf64d3fb4ce07c8d1e48229b3aa39
flandre scarlet <flantan233@gmail.com>
2018-05-30 11:33:40 +0800

    update class group & init class list

---
+++ src/components/ClassList/ClassList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="class-list">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "class-list",
+    data() {
+      return {
+        listData : [
+          {
+            thumb: require('./images/thumb1.png'),
+            type: "车辆知识",
+            title: "车辆结构与行驶原理",
+            time: '05:31/05:31',
+            score: '100/100',
+            isComplete: true
+          },
+          {
+            thumb: require('./images/thumb1.png'),
+            type: "车辆知识",
+            title: "车辆结构与行驶原理",
+            time: '00:00/04:30',
+            score: '80/100',
+            isComplete: true
+          },
+          {
+            thumb: require('./images/thumb1.png'),
+            type: "车辆知识",
+            title: "车辆结构与行驶原理",
+            time: '01:00/06:30',
+            score: '70/100',
+            isComplete: true
+          },
+        ]
+      }
+    },
+    props: {
+      title: {
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .class-list {
+
+  }
+</style>
--- src/pages/ClassGroup.vue
+++ src/pages/ClassGroup.vue
@@ -1,15 +1,60 @@
 <template>
   <div class="class-group">
-    这是视频列表
+    <NavigatorBar title="" bgColor="#fff" navType="dark" scrollType="fade" navTypeChange="change"/>
+    <img src="../assets/pages/ClassGroup/banner.png" class="banner">
+    <div class="group-info">
+      <p class="title">新手上路宝典</p>
+      <p class="text">很多车主认为，安全带就是“拉过来、扣上去”这么简单。虽然它只是一根简单的可以扣起来的带子,但在关键时刻能救你一命。</p>
+    </div>
+    <div class="group-list">
+
+    </div>
   </div>
 </template>
 
 <script>
+  import NavigatorBar from '@/components/NavigatorBar/NavigatorBar'
   export default {
-    name: "class-group"
+    name: "class-group",
+    components: {
+      NavigatorBar,
+    },
   }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .class-group {
+    .banner {
+      width: 100%;
+      height: auto;
+      display: block;
+    }
+    .group-info {
+      background: #fff;
+      padding: 20px 15px;
+      position: relative;
+      overflow: hidden;
+      .title {
+        font-size: 15px;
+        line-height: 15px;
+        color: #333;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 13px;
+        color: #666666;
+        line-height: 18px;
+      }
+      &:after {
+        width: 100%;
+        height: 1px;
+        position: absolute;
+        bottom: 0;
+        left: 15px;
+        background: #dbdbdb;
+      }
+
+    }
+  }
 
 </style>
