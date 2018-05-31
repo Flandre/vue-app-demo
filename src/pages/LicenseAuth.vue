5ae307787c52561b35312836ca5446a9f6b19499
flandre scarlet <flantan233@gmail.com>
2018-05-31 18:33:42 +0800

    init page

---
+++ src/pages/LicenseAuth.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="license-auth">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "license-auth"
+  }
+</script>
+
+<style lang="less" scoped>
+  .license-auth {
+
+  }
+</style>
